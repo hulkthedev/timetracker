228bf7ae0885159cb38e46060a6c856e47a89b37
= <=>
2020-04-03 17:25:19 +0200

    try to fix sonarcloud

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # install composer
-docker run --rm -v $(pwd):/app composer/composer install --prefer-dist --ignore-platform-reqs --optimize-autoloader
+docker run --rm -v $(pwd):/app composer:1.10.1 install --prefer-dist --ignore-platform-reqs --optimize-autoloader
 
 # start container
 docker-compose up -d
